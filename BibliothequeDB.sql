4bb479d540d064708d6116655e5d99446cfed0a4
anf692 <fassane348@gmail.com>
2026-02-25 09:18:52 +0000

    Ajout du fichier dump

---
+++ BibliothequeDB.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Linux (x86_64)
+--
+-- Host: localhost    Database: Biblio_Tech
+-- ------------------------------------------------------
+-- Server version	8.0.45-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `documents` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(100) NOT NULL,
+  `type_doc` enum('livre','magazine') NOT NULL,
+  `disponible` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `documents`
+--
+
+LOCK TABLES `documents` WRITE;
+/*!40000 ALTER TABLE `documents` DISABLE KEYS */;
+INSERT INTO `documents` VALUES (1,'Une si longue lettre','livre',0);
+/*!40000 ALTER TABLE `documents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `emprunts`
+--
+
+DROP TABLE IF EXISTS `emprunts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `emprunts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `membre_id` int NOT NULL,
+  `document_id` int NOT NULL,
+  `date_emprunt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `date_retour` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `membre_id` (`membre_id`),
+  KEY `document_id` (`document_id`),
+  CONSTRAINT `emprunts_ibfk_1` FOREIGN KEY (`membre_id`) REFERENCES `membres` (`id_membre`) ON DELETE CASCADE,
+  CONSTRAINT `emprunts_ibfk_2` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `emprunts`
+--
+
+LOCK TABLES `emprunts` WRITE;
+/*!40000 ALTER TABLE `emprunts` DISABLE KEYS */;
+INSERT INTO `emprunts` VALUES (1,1,1,'2026-02-24 21:53:51',NULL);
+/*!40000 ALTER TABLE `emprunts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `livres`
+--
+
+DROP TABLE IF EXISTS `livres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `livres` (
+  `document_id` int NOT NULL,
+  `auteur` varchar(100) NOT NULL,
+  PRIMARY KEY (`document_id`),
+  CONSTRAINT `livres_ibfk_1` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `livres`
+--
+
+LOCK TABLES `livres` WRITE;
+/*!40000 ALTER TABLE `livres` DISABLE KEYS */;
+INSERT INTO `livres` VALUES (1,'Mariama BA');
+/*!40000 ALTER TABLE `livres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `magazines`
+--
+
+DROP TABLE IF EXISTS `magazines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `magazines` (
+  `document_id` int NOT NULL,
+  `numero` int NOT NULL,
+  PRIMARY KEY (`document_id`),
+  CONSTRAINT `magazines_ibfk_1` FOREIGN KEY (`document_id`) REFERENCES `documents` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `magazines`
+--
+
+LOCK TABLES `magazines` WRITE;
+/*!40000 ALTER TABLE `magazines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `magazines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `membres`
+--
+
+DROP TABLE IF EXISTS `membres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `membres` (
+  `id_membre` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_membre`),
+  UNIQUE KEY `nom` (`nom`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `membres`
+--
+
+LOCK TABLES `membres` WRITE;
+/*!40000 ALTER TABLE `membres` DISABLE KEYS */;
+INSERT INTO `membres` VALUES (1,'assane');
+/*!40000 ALTER TABLE `membres` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-25  9:18:26
