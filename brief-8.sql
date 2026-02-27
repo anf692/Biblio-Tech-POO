6d976bce455c030aae4c9e6ec9ae1d4b40dd6f1f
anf692 <fassane348@gmail.com>
2026-02-27 09:05:30 +0000

    Ajout derniere reglage

--- brief-8.sql
+++ brief-8.sql
@@ -41,5 +41,13 @@ CREATE TABLE emprunts (
     FOREIGN KEY (document_id) REFERENCES documents(id) 
 );
 
-select * from documents;
+CREATE TABLE utilisateurs (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+select * from utilisateurs;
 
